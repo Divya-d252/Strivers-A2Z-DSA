cb4ad7c3139c8c1aaff6afc47b95c4c2b5855194
Divya-d252 <98879211+Divya-d252@users.noreply.github.com>
2022-10-23 12:00:48 +0530

    Create 4. switch_statement.py

---
+++ Step 1. Learn the Basics/Step 1.1 : Things to Know in any language/4. switch_statement.py
@@ -0,0 +1,7 @@
+import math
+class Solution:
+    def switchCase(self, choice, arr):
+        if(choice == 1):
+            return (math.pi)*arr[0]*arr[0]
+        else:
+            return arr[0]*arr[1]
