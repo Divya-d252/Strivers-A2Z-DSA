f49b900198b448b260ffde78418fd5a4375d4205
Divya-d252 <98879211+Divya-d252@users.noreply.github.com>
2022-10-23 12:07:42 +0530

    Create 7. while_loops.java

---
+++ Step 1. Learn the Basics/Step 1.1 : Things to Know in any language/7. while_loops.java
@@ -0,0 +1,14 @@
+//User function Template for Java
+class Geeks {
+	 static void printTable (int n) 
+	 {
+            int multiplier=10;
+            while(multiplier>0)
+            {
+                System.out.print(n*multiplier+" ");
+                multiplier=multiplier-1;
+            }
+       System.out.println();
+	 }
+
+}
