3cf0cb38971db9fc560eb3528a5a66b1204e568b
Divya-d252 <98879211+Divya-d252@users.noreply.github.com>
2022-10-23 21:46:15 +0530

    Create pattern_19.py

---
+++ Step 1. Learn the Basics/Step 1.2 : Build-up Logical Thinking/pattern_19.py
@@ -0,0 +1,32 @@
+# Input: 5
+
+# Output:
+# **********
+# ****  ****
+# ***    ***
+# **      **
+# *        *
+# *        *
+# **      **
+# ***    ***
+# ****  ****
+# **********
+
+class Solution:
+    def printTriangle(self, N):
+        for i in range(N):
+            for j in range(2*N):
+                if(i+j<N):
+                    print("*",end="")
+                elif(j>=N-1 and j-N-1>=i-1):
+                    print("*",end="")
+                else:
+                    print(" ",end="")
+            print()
+        for i in range(N):
+            for j in range(2*N):
+                if(i>=j or i+j>=(2*N-1)):
+                    print("*",end="")
+                else:
+                    print(" ",end="")
+            print()
